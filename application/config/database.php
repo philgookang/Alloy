4905b8c5de100b1b41352e4a9a969c447bfd1af0
Phil Goo Kang <philgookang@gmail.com>
2017-08-21 22:31:03 +0900

    added database access information

--- application/config/database.php
+++ application/config/database.php
@@ -1,3 +1,8 @@
 <?php
 
 $config = array();
+$config['host']         = '';
+$config['userid']       = '';
+$config['password']     = '';
+$config['database']     = '';
+$config['log_setting']  = false;
