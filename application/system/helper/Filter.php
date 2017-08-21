b809331ab24717ef44826a4050068e60527c59eb
Phil Goo Kang <philgookang@gmail.com>
2017-08-21 15:53:08 +0900

    Split "hash_array" function, added "clean_array" function that cleans the index of the array

--- application/system/helper/Filter.php
+++ application/system/helper/Filter.php
@@ -1,10 +1,15 @@
 <?php
 
 function hash_array($param_list = array()) {
+    $list = clean_array($param_list);
+    $serialize = serialize($list);
+    return md5($serialize);
+}
+
+function clean_array($param_list = array()) {
     $list = array();
     foreach($param_list as $val) {
         array_push($list, $val);
     }
-    $serialize = serialize($list);
-    return md5($serialize);
+    return $list;
 }
