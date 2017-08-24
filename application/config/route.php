326f752c7a1107811918eadcf1d28a4bc7a8c669
Phil Goo Kang <philgookang@gmail.com>
2017-08-24 18:13:58 +0900

    added instruction to route cofnig

--- application/config/route.php
+++ application/config/route.php
@@ -1,4 +1,25 @@
 <?php
 
 $config = array();
+
+/*
+|--------------------------------------------------------------------------
+| Default Route
+|--------------------------------------------------------------------------
+|
+| The default route when the user visits the index.html page
+|
+*/
 $config['default'] = 'hello';
+
+/*
+|--------------------------------------------------------------------------
+| Sample Route
+|--------------------------------------------------------------------------
+|
+| You can add an alias to existing route's
+| However, do to the flexibility setting route's
+| This is a useless feature.
+|
+*/
+// $config['sample'] = 'route';
