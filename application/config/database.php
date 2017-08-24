582b5881747dd80d6f71d4c6af9e5589844edc64
Phil Goo Kang <philgookang@gmail.com>
2017-08-24 18:12:59 +0900

    added description to database config

--- application/config/database.php
+++ application/config/database.php
@@ -1,6 +1,17 @@
 <?php
 
 $config = array();
+
+/*
+|--------------------------------------------------------------------------
+| Database Information
+|--------------------------------------------------------------------------
+|
+| Place your database access information here
+| While this framework does not offer any database class,
+| You can use the Postman (https://github.com/philgookang/Postman) Database Class
+|
+*/
 $config['host']         = '';
 $config['userid']       = '';
 $config['password']     = '';
