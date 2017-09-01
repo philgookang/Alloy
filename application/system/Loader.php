d8941a9d71be164348a77511174c01be3f321963
Phil Goo Kang <philgookang@gmail.com>
2017-09-01 15:36:41 +0900

    added new socket server feature check to map

--- application/system/Loader.php
+++ application/system/Loader.php
@@ -3,9 +3,34 @@
 class Loader {
 
     private $viewer;
+    private $ws;
+    public $wc;
 
     public function __construct() {
+
+        $alloy = Alloy::init();
+
         $this->viewer = new Viewer();
+
+        // check if socket is enabled
+        if ($alloy->config['socket']['enabled']) {
+
+            if (!is_cli()) {
+                die('CLI Access is prohibited');
+            }
+
+            $this->ws = new WebSocket($alloy->config['socket']['host'], $alloy->config['socket']['port']);
+
+            try {
+                $this->ws->run();
+            }
+            catch (Exception $e) {
+                $this->ws->stdout($e->getMessage());
+            }
+            ////////////////////////////////////////////////////
+        } else {
+            $this->wc = new WebConnect();
+        }
     }
 
     public function view($name, $filename = '', $target, $data = array()) {
