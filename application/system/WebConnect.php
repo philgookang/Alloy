9e2ad0edd1e91b7f40428ad5e8161c93eb36bdc7
Phil Goo Kang <philgookang@gmail.com>
2017-09-01 15:37:18 +0900

    new socket server component

---
+++ application/system/WebConnect.php
@@ -0,0 +1,30 @@
+<?php
+
+class WebConnect {
+
+    public function getAccessUrl() {
+
+        // get alloy instance
+        $alloy = Alloy::init();
+
+        // url uri
+        $uri = ($_SERVER['REQUEST_URI'] == '/') ? $alloy->config['route']['default'] : $_SERVER['REQUEST_URI'];
+
+        // get request method
+        $REQUEST_METHOD = (isset($_SERVER["REQUEST_METHOD"])) ? $_SERVER["REQUEST_METHOD"] : 'GET';
+
+
+        // get uri list
+        $uri_list = array_filter(explode('/', $uri));
+
+        // get hash key
+        $key_list = clean_array($uri_list);
+
+
+        // add request method to key list
+        array_unshift($key_list, $REQUEST_METHOD);
+
+        //get key list
+        return $key_list;
+    }
+}
---
+++ application/system/vendor/WebSocket/autoload.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once('WebSocketUser.php');
+require_once('WebSocketServer.php');
+require_once('WebSocket.php');
