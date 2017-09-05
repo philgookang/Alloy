57448469d9b47588e37918c76b446c52f7ab4973
Phil Goo Kang <philgookang@gmail.com>
2017-09-05 11:49:15 +0900

    added new addon_array

--- application/system/helper/Filter.php
+++ application/system/helper/Filter.php
@@ -9,7 +9,19 @@ function hash_array($param_list = array()) {
 function clean_array($param_list = array()) {
     $list = array();
     foreach($param_list as $val) {
-        array_push($list, $val);
+        if ($val != "") {
+            array_push($list, $val);
+        }
     }
+    return $list;
+}
+
+function addon_array($original_array, $new_value) {
+    $list = array();
+    for($i = 0; $i < count($original_array); $i++) {
+        array_push($list, $original_array[$i]);
+    }
+
+    array_push($list, $new_value);
     return $list;
 }
