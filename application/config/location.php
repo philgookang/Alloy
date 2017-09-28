132780ff2acfcd9540557b4254e8f472fe944f54
Phil Goo Kang <philgookang@gmail.com>
2017-09-28 20:31:10 +0900

    added a config setting to collapse index

--- application/config/location.php
+++ application/config/location.php
@@ -1,3 +1,13 @@
 <?php
 
 $config = array();
+
+/*
+|--------------------------------------------------------------------------
+| Collapse Index
+|--------------------------------------------------------------------------
+|
+| Allow missing variable in URL to match the index function
+|
+*/
+$config['collapse_index'] = false;
